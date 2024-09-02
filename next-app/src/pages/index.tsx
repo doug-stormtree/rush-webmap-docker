7ea43663168c2fbe553f7c733ffb165e769e0684
Samuel Morris <53889741+dodobird181@users.noreply.github.com>
2024-09-01 20:58:30 -0400

    test changes for hot reloading

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next app</title>
+        <title>What's The RUSH?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker Compose!
+          Welcome to What's the RUSH on Docker Compose!
         </h1>
 
         <p className={styles.description}>
